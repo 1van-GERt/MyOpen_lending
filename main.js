f52bbaf3b38447e59f1ba5f4fc34e2438b7da72e
Ivan <80355099+1van-GERt@users.noreply.github.com>
2023-11-15 23:08:30 +0300

    Add files via upload
    add js

--- main.js
+++ main.js
@@ -1,46 +1,53 @@
 const body = document.body;
-const burger = document.getElementById('burger');
+const header_burger = document.getElementById('header_burger');
 const btn_closeMenu = document.getElementById('btn_closeMenu');
-const mask = document.getElementById('mask');
-const menu_links = document.getElementsByClassName('menu_link');
+const mobile_nav_container = document.getElementById('mobile_nav_container');
+const mobile_nav = document.getElementById('mobile_nav');
+const mobile_nav_link = document.getElementsByClassName('mobile_nav_link');
 
-burger.addEventListener('click', event =>{
+console.log(mobile_nav_container)
 
-	if( body.classList.contains('show_menu')){
+header_burger.addEventListener('click', event =>{
+
+	if( body.classList.contains('show_mobile_menu')){
 		closeMenu();
 	}	else{
 		showMenu();
 	}
 });
 
-btn_closeMenu.addEventListener('click', event =>{
+mobile_nav.addEventListener('click', event =>{
 
-	if( body.classList.contains('show_menu')){
+	event.stopPropagation()
+});
+// btn_closeMenu.addEventListener('click', event =>{
+
+// 	if( body.classList.contains('show_mobile_menu')){
+// 		closeMenu();
+// 	}	else{
+// 		showMenu();
+// 	}
+// });
+
+mobile_nav_container.addEventListener('click', event =>{
+	console.log('win')
+	if( body.classList.contains('show_mobile_menu')){
 		closeMenu();
 	}	else{
 		showMenu();
 	}
 });
 
-mask.addEventListener('click', event =>{
-
-	if( body.classList.contains('show_menu')){
-		closeMenu();
-	}	else{
-		showMenu();
-	}
-});
-
-for (let i = 0; i < menu_links.length; i++) {
-	menu_links[i].addEventListener('click', event =>{
+for (let i = 0; i < mobile_nav_link.length; i++) {
+	mobile_nav_link[i].addEventListener('click', event =>{
 		closeMenu();
 	});
 }
 
 function showMenu(){
-	body.classList.add('show_menu');
+	body.classList.add('show_mobile_menu');
 };
 
 function closeMenu(){
-	body.classList.remove('show_menu');
+	body.classList.remove('show_mobile_menu');
 };
